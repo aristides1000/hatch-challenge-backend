d665b86bd411d78f0091f6440fedbea5b36d3026
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-12-13 15:55:29 -0400

    Add more test

--- spec/controllers/user_spec.rb
+++ spec/controllers/user_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'LoggedInUserController', type: :request do
-  describe 'Reject the user without authentication or token' do
-    it 'returns http success' do
+  describe 'GET /users' do
+    it 'let you create a new user' do
+      post '/users', params: { :user => { name: "Daniel", email: "daniel@mail.com", password: "123456", password_confirmation: "123456" } }
+      expect(User.all.length).to be(1)
+      expect(JSON.parse(response.body)['message'] == 'Signed up sucessfully.').to be(true)
+    end
+    it 'only one email per account' do
+      post '/users', params: { :user => { name: "Daniel", email: "daniel@mail.com", password: "123456", password_confirmation: "123456" } }
+      post '/users', params: { :user => { name: "jaar", email: "daniel@mail.com", password: "123456", password_confirmation: "123456" } }
+      expect(User.all.length).to be(1)
+    end
+    it 'let you log if user exist' do
+      post '/users', params: { :user => { name: "Daniel", email: "daniel@mail.com", password: "123456", password_confirmation: "123456" } }
+      post '/users/sign_in', params: { user: {"email": "daniel@mail.com", "password": "123456"}}
+      expect(JSON.parse(response.body)['message'] == "You are logged in.").to eq(true)
+    end
+    it 'Reject the user without authentication or token' do
       get '/logged_user', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }
       expect(JSON.parse(response.body)['error'] == 'You need to sign in or sign up before continuing.').to be(true)
     end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -25,6 +25,14 @@ RSpec.describe Address, type: :model do
         address.city = nil
         expect(address).to_not be_valid
       end
+
+      it 'user has many addresses' do
+        user = User.create(name:"jaar", email:"jaar@gmail.com", password:"password")
+        user.addresses.create(street:"test", external_number: "test", internal_number: "test", postal_code: "12345", colony: "test",
+        municipality: "test", city:"test", state:"test", country:"test" )
+        expect(Address.all.length).to eq(1)
+        expect(user.addresses.length).to eq(1)
+       end
     end
   end
 end
